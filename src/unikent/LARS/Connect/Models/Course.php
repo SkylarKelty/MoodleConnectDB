dc915c2179932ce576afe835b7ad73ad351a2804
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-01-13 14:45:47 +0000

    Fix

--- src/unikent/LARS/Connect/Models/Course.php
+++ src/unikent/LARS/Connect/Models/Course.php
@@ -14,13 +14,13 @@ namespace unikent\LARS\Connect\Models;
 class Course {
 	public static function migrate() {
 		$table = new \SkylarK\Fizz\Util\FizzMigrate("course_list");
-		$table->addField("id", "bigint(11)");
+		$table->addField("id", "bigint(11)", true); // TODO - auto increment
 		$table->addField("moodle_key", "varchar(24)");
 		$table->addField("moodle_id", "bigint(11)");
 		$table->addField("shortname", "varchar(255)");
 		$table->addField("fullname", "varchar(254)");
 		$table->addField("summary", "longtext", true);
-		$table->setPrimary("id", true);
+		$table->setPrimary("id");
 		$table->commit();
 	}
 }
